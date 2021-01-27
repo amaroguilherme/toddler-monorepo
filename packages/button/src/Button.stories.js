40349796e469f67c8f948aedb7f31fad5fb4338c
Guilherme Alves <guilherme.amaro@ibm.com>
2021-01-27 19:04:17 -0300

    creates monorepo infrastructure

---
+++ packages/button/src/Button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from './packages/button/src/Button.js';
+export default {
+  component: Button,
+  title: 'Design System/Button'
+};
+export const primary = () => <Button>{'Button'}</Button>;
